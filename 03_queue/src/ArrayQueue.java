3a2df491925c982a1f24ea68e0344dd727a0ebd1
shenwl <shenweilun96@gmail.com>
2018-06-18 21:35:29 +0800

    数组队列(性能极差)

--- 03_queue/src/ArrayQueue.java
+++ 03_queue/src/ArrayQueue.java
@@ -56,4 +56,17 @@ public class ArrayQueue<E> implements Queue<E> {
         res.append("] tail");
         return res.toString();
     }
+
+    public static void main(String[] args) {
+        LinkedListQueue<Integer> queue = new LinkedListQueue<Integer>();
+        for (int i = 0; i < 10; i++) {
+            queue.enqueue(i);
+            System.out.println(queue);
+
+            if (i % 3 == 2) {
+                queue.dequeue();
+                System.out.println(queue);
+            }
+        }
+    }
 }
