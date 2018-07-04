64a453fac04c5ad3339c1d2067e132c9f85e6e54
shenwl <shenweilun96@gmail.com>
2018-07-04 13:12:12 +0800

    精简代码

--- 05_recursion/src/Solution2.java
+++ 05_recursion/src/Solution2.java
@@ -1,4 +1,4 @@
-// 使用虚拟头结点
+// LeetCode 203题 使用虚拟头结点
 public class Solution2 {
 
     public ListNode removeElements(ListNode head, int val) {
@@ -16,5 +16,14 @@ public class Solution2 {
         }
 
         return dummyHead.next;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {1, 2, 6, 3, 4, 5, 6};
+        ListNode head = new ListNode(nums);
+        System.out.println(head);
+
+        ListNode res = (new Solution()).removeElements(head, 6);
+        System.out.println(res);
     }
 }
